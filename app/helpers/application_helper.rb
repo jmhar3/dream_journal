dbf83dc69f0d5cabc666a91dd654479c9e576044
jmhar <jmhar@protonmail.com>
2021-10-21 10:46:16 +1100

    find current date/month, custom header for tracker pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
     def menu_icon display
         content_for :menu_icon, display
     end
+
+    def tracker_title title
+        content_for :tracker_title, title
+    end
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,11 @@
 module PagesHelper
+    def current_date
+        date = DateTime.now
+        date.strftime("%A, #{date.day.ordinalize} of %B")
+    end
+
+    def current_month
+        date = DateTime.now
+        date.strftime("%B")
+    end
 end
