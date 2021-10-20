d6bd8393ea46ecb1a369e7f93631c5591a82df70
jmhar <jmhar@protonmail.com>
2021-10-20 17:47:04 +1100

    basic styling + home page set up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+    def menu_icon display
+        content_for :menu_icon, display
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "pages#home"
+
+  resources :pages
+  resources :finances
+  resources :goals
+  resources :gratitudes
+  resources :meals
+  resources :notes
+  resources :self_cares
+  resources :users
 end
