67460d0bc06ea1678e41614a68443656d57de6e6
jmhar <jmhar@protonmail.com>
2021-10-21 16:55:22 +1100

    gratitude form + styling + fix header button, divider, layout

--- app/controllers/gratitudes_controller.rb
+++ app/controllers/gratitudes_controller.rb
@@ -1,2 +1,31 @@
 class GratitudesController < ApplicationController
+    def new
+        @gratitude = Gratitude.new
+    end
+
+    def create
+        @gratitude = Gratitude.new(gratitude_params)
+        if @gratitude.save
+            redirect_to daily_path
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @gratitude = Gratitude.find(gratitude_params[:id])
+    end
+
+    def update
+        @gratitude = Gratitude.find(gratitude_params[:id])
+        @gratitude.update(gratitude_params)
+        
+        redirect_to daily_path
+    end
+
+    private
+
+    def gratitude_params
+        params.require(:gratitude).permit(:name)
+    end
 end
