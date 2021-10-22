8c6fb59455b1f6763287e0a12cf679d7269ccb8c
jmhar <jmhar@protonmail.com>
2021-10-22 14:29:13 +1100

    set up self care form

--- app/controllers/self_cares_controller.rb
+++ app/controllers/self_cares_controller.rb
@@ -1,2 +1,33 @@
 class SelfCaresController < ApplicationController
+    def new
+        @self_care = SelfCare.new
+    end
+
+    def create
+        @self_care = SelfCare.new(self_care_params)
+        if @self_care.save
+            redirect_to daily_path
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @self_care = SelfCare.find(self_care_params[:id])
+    end
+
+    def update
+        @self_care = SelfCare.find(self_care_params[:id])
+        @self_care.update(self_care_params)
+        
+        redirect_to daily_path
+    end
+
+    private
+
+    def self_care_params
+        params.require(:self_cares)
+        .permit(:user_id, :label, :goal, :frequency, :completed)
+        .with_defaults(user_id: session[:user_id], completed: 0)
+    end
 end
--- app/helpers/self_cares_helper.rb
+++ app/helpers/self_cares_helper.rb
@@ -1,2 +1,9 @@
 module SelfCaresHelper
+    def frequency_options
+        [
+            ['Day', 'day'],
+            ['Week', 'week'],
+            ['Month', 'month']
+        ]
+    end
 end
