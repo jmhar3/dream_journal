cbf8c528ca51bd25c2773ba604fa3171d01d6828
jmhar <jmhar@protonmail.com>
2021-10-29 19:44:23 +1100

    fix update details

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 6}
+    validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 5}
     validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
 
     has_many :finances
