46a4a963b030a8f0e72dba4433ef826d55845af3
Jessica Harriman <jmhar@protonmail.com>
2021-11-22 13:30:51 +1100

    convert tracker menu page into pop up

--- app/javascript/packs/toggleMenu.js
+++ app/javascript/packs/toggleMenu.js
@@ -1,10 +1,9 @@
 document.getElementById("menu").addEventListener('click', toggleDropdown)
 
 function toggleDropdown(e) {
-    console.log('bye')
     if (e.target.classList.contains('toggle_menu')) {
         document.getElementById("nav-dropdown").classList.toggle("hide");
+    } else if (e.target.classList.contains('toggle_tracker_menu')) {
+        document.getElementById("tracker-dropdown").classList.toggle("hide");
     }
 }
-
-console.log('hi')
